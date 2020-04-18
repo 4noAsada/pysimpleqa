a37004efacbd10278703bd23628bae55c25e827e
4noAsada <xfreunion@live.com>
2020-04-18 21:05:59 +0800

    completed all the code ^^

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import sys
+
+
+def start(filename):
+    with open(filename, encoding='utf-8') as file:
+        lines = file.readlines()
+        for index in range(len(lines), step=2):
+            print(lines[index])
+            answer = input('(\\a for answer) >>> ')
+            while answer != lines[index]:
+                if answer == "\\a":
+                    print(lines[index + 1])
+                else:
+                    answer = input('(wrong; \\a for answer) >>> ')
+
+
 if __name__ == "__main__":
-    print('Hi')
+    for filename in sys.argv[1:]:
+        start(filename)
